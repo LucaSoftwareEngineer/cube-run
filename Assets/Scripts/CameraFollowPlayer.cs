f983e7e3eaef3ab8f74ccb295d692fca21b9a780
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-12-29 14:17:02 +0100

    fix: remove unused methods

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -7,18 +7,6 @@ public class CameraFollowPlayer : MonoBehaviour
     public Transform player;
     public Vector3 offset;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void LateUpdate()
     {
         float xFissa = offset.x;
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,18 +8,6 @@ public class PlayerMovement : MonoBehaviour
     public float spintaInAvanti;
     public float spintaLaterale;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
 
     void FixedUpdate()
     {
